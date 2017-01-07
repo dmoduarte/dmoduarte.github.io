bd7b3438e82505025e9ebe45d2d4def5ded6bf78
dmo.duarte <dmo.duarte@gmail.com>
2017-01-07 16:47:28 +0000

    Implemented tabs

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+    
+    $(".carousel").carousel();
 
-$(".carousel").carousel();
+    $(".topnav ul").on('click','a',function(el){        
+       
+       $(".topnavbutton.active").removeClass("active");
+       $(this).addClass("active");
+       $(".body>div.active").removeClass("active");
+       $("."+$(this).data().tab).addClass("active");
+        
+    });
 
+});
