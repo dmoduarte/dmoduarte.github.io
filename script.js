88207d2ec6c9783c49f1a8e5bd754f44773eca95
dmo.duarte <dmo.duarte@gmail.com>
2017-01-07 21:03:53 +0000

    Responsive header

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ $(document).ready(function() {
     
     $(".carousel").carousel();
 
-    $(".topnav ul").on('click','a',function(el){        
+    $(".topnav ul").on('click','a',function(e){        
        
        $(".topnavbutton.active").removeClass("active");
        $(this).addClass("active");
        $(".body>div.active").removeClass("active");
        $("."+$(this).data().tab).addClass("active");
         
+    });
+    
+     $("#show-more").on('click','a',function(e){        
+       
+       $(".topnavbutton.active").removeClass("active");
+       $(this).addClass("active");
+       $(".body>div.active").removeClass("active");
+       $("."+$(this).data().tab).addClass("active");
+       $("#show-more").removeClass("active");  
+         
+    });
+    
+    $('a[href="#show-more"]').on('click',function(){   
+        $("#show-more").toggleClass("active");
     });
 
 });
