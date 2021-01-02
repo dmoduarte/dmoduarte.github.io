d28ccd2f66c22abd3c8d93068d988bb3d7131eda
dmoduarte <dmo.duarte@gmail.com>
2021-01-02 15:27:19 +0000

    Changed icons, added flex items

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     $(".carousel").carousel();
 
-    $(".tab-opener").on('click', function(e){        
-       
-       $(".topnavbutton.active").removeClass("active");
-       $("#"+$(this).attr('id')+".topnavbutton").addClass("active");
-       $(".body>div.active").removeClass("active");
-       $("."+$(this).data().tab).addClass("active");
-        
+    $(".tab-opener").on('click', function (e) {
+
+        $(".topnavbutton.active").removeClass("active");
+        $("#" + $(this).attr('id') + ".topnavbutton").addClass("active");
+        $(".body>div.active").removeClass("active");
+        $("." + $(this).data().tab).addClass("active");
+
     });
-    
-     $("#show-more").on('click','a',function(e){        
-       
-       $(".topnavbutton.active").removeClass("active");
-       $(this).addClass("active");
-       $(".body>div.active").removeClass("active");
-       $("."+$(this).data().tab).addClass("active");
-       $("#show-more").removeClass("active");  
-         
+
+    const showMore = $("#show-more");
+
+    //This handles the user-click on the mobile menu
+    $('a[href="#show-more"]').on('click', function () {
+        showMore.toggleClass("active");
     });
-    
-    $('a[href="#show-more"]').on('click',function(){   
-        $("#show-more").toggleClass("active");
-    });
+
+    //This handles the click on the mobile menu item
+   /* showMore.on('click', 'a', function (e) {
+
+        $(".topnavbutton.active").removeClass("active");
+        $(this).addClass("active");
+        $(".body>div.active").removeClass("active");
+        $("." + $(this).data().tab).addClass("active");
+        showMore.removeClass("active");
+
+    });*/
 
 });
